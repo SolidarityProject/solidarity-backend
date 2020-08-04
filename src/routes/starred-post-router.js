65d4d27e1ecd2ee534e78d94aeecf8dda87f5a55
MustafaCEVIK <cevikksoft@gmail.com>
2020-08-04 19:00:05 +0300

    Add error handling & empty check -> routes/starred-post-router.

--- src/routes/starred-post-router.js
+++ src/routes/starred-post-router.js
@@ -7,12 +7,20 @@ const middleware = require("../middlewares/auth");
 const router = express.Router();
 
 //* getbyid
-router.get("/getbyid/:starredPostId", middleware.auth, async (req, res) => {
+router.get("/getbyid/:starredPostId", async (req, res) => {
   try {
-    const starredPost = await StarredPost.findById(req.params.starredPostId);
-    const user = await User.findById(starredPost.userId);
-    const post = await Post.findById(starredPost.postId);
-    res.status(200).send({ user: user, post: post });
+    await StarredPost.findById(
+      req.params.starredPostId,
+      async (err, starredPost) => {
+        if (err) return res.status(500).send(err);
+
+        if (starredPost) {
+          const user = await User.findById(starredPost.userId);
+          const post = await Post.findById(starredPost.postId);
+          res.status(200).send({ user: user, post: post });
+        } else return res.status(400).send("Starred Post not found.");
+      }
+    );
   } catch (error) {
     res.status(500).send(error);
   }
@@ -21,14 +29,22 @@ router.get("/getbyid/:starredPostId", middleware.auth, async (req, res) => {
 //* getbyuserid
 router.get("/getbyuserid/:userId", middleware.auth, async (req, res) => {
   try {
-    const starredPosts = await StarredPost.find({ userId: req.params.userId });
-    const posts = [];
+    await StarredPost.find(
+      { userId: req.params.userId },
+      async (err, starredPosts) => {
+        if (err) res.status(500).send(err);
 
-    for (let index = 0; index < starredPosts.length; index++) {
-      const post = await Post.findById(starredPosts[index].postId);
-      posts.push(post);
-    }
-    res.status(200).send(posts);
+        if (starredPosts.length) {
+          const posts = [];
+
+          for (let index = 0; index < starredPosts.length; index++) {
+            const post = await Post.findById(starredPosts[index].postId);
+            posts.push(post);
+          }
+          res.status(200).send(posts);
+        } else return res.status(400).send("Starred Post not found.");
+      }
+    );
   } catch (error) {
     res.status(500).send(error);
   }
@@ -37,14 +53,22 @@ router.get("/getbyuserid/:userId", middleware.auth, async (req, res) => {
 //* getbypostid
 router.get("/getbypostid/:postId", async (req, res) => {
   try {
-    const starredPosts = await StarredPost.find({ postId: req.params.postId });
-    const users = [];
+    await StarredPost.find(
+      { postId: req.params.postId },
+      async (err, starredPosts) => {
+        if (err) return res.status(500).send(err);
 
-    for (let index = 0; index < starredPosts.length; index++) {
-      const user = await User.findById(starredPosts[index].userId);
-      users.push(user);
-    }
-    res.status(200).send(users);
+        if (starredPosts.length) {
+          const users = [];
+
+          for (let index = 0; index < starredPosts.length; index++) {
+            const user = await User.findById(starredPosts[index].userId);
+            users.push(user);
+          }
+          res.status(200).send(users);
+        } else return res.status(400).send("Starred Post not found.");
+      }
+    );
   } catch (error) {
     res.status(500).send(error);
   }
@@ -75,11 +99,18 @@ router.delete("/delete", middleware.auth, async (req, res) => {
   //if (error) return res.status(400).send(error.details[0].message);
 
   try {
-    const starredPost = await StarredPost.findOneAndDelete({
-      userId: req.user._id,
-      postId: req.body.postId,
-    });
-    res.status(200).send(starredPost);
+    await StarredPost.findOneAndDelete(
+      {
+        userId: req.user._id,
+        postId: req.body.postId,
+      },
+      async (err, starredPost) => {
+        if (err) return res.status(500).send(err);
+
+        if (starredPost) res.status(200).send(starredPost);
+        else return res.status(400).send("Starred Post not found.");
+      }
+    );
   } catch (error) {
     res.status(500).send(error);
   }
