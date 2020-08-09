37e6e69b55562ef81afa7323b398c1f6171b7aca
MustafaCEVIK <cevikksoft@gmail.com>
2020-08-09 19:49:29 +0300

    Complete starred-router.test functions.

--- test/routes/iv-starred-router.test.js
+++ test/routes/iv-starred-router.test.js
@@ -66,7 +66,60 @@ describe("Starred Router Test Functions", () => {
       .end((error, response) => {
         expect(response.status).to.equal(200);
         expect(response.body).to.be.an.instanceof(Array);
-        expect(response.body[0]).to.equal(user1.postId[0]);
+        expect(response.body[0]).to.equal(user1._id);
+        done();
+      });
+  });
+
+  //* testing getpostsbyuserid
+  it("GET : getpostsbyuserid", (done) => {
+    chai
+      .request(server)
+      .get("/starred/getpostsbyuserid/" + user1._id)
+      .set("token", user1.token)
+      .end((error, response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an.instanceof(Array);
+        expect(response.body[0]._id).to.equal(user1.postId[0]);
+        done();
+      });
+  });
+
+  //* testing getusersinfobypostid
+  it("GET : getusersinfobypostid", (done) => {
+    chai
+      .request(server)
+      .get("/starred/getusersinfobypostid/" + user1.postId[0])
+      .set("token", user1.token)
+      .end((error, response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an.instanceof(Array);
+        expect(response.body[0]._id).to.equal(user1._id);
+        done();
+      });
+  });
+
+  //* testing delete
+  it("DEL : delete", (done) => {
+    chai
+      .request(server)
+      .delete("/starred/delete/" + user1.postId[0])
+      .set("token", user1.token)
+      .end((error, response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an.instanceof(Array);
+        done();
+      });
+  });
+
+  //* testing delete error because this post non starred
+  it("DEL : delete (error)", (done) => {
+    chai
+      .request(server)
+      .delete("/starred/delete/" + user1.postId[0])
+      .set("token", user1.token)
+      .end((error, response) => {
+        expect(response.status).to.equal(400);
         done();
       });
   });
