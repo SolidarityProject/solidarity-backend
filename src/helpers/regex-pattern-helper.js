8ca8a8e44bddf31550a5cb4523f7244ff483a6d1
MustafaCEVIK <cevikksoft@gmail.com>
2020-08-20 23:52:21 +0300

    Edit email regex pattern -> regex-pattern-helper.

--- src/helpers/regex-pattern-helper.js
+++ src/helpers/regex-pattern-helper.js
@@ -1,6 +1,6 @@
 const usernameRegexPattern = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/;
 
-const emailRegexPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const emailRegexPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,3}$/;
 
 module.exports = {
   usernameRegexPattern,
